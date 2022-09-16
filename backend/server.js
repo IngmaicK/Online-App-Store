ee623af5b1e6ee4072a29661548e87476bc2b0eb
Miguel <ingmaick@gmail.com>
2022-09-16 14:09:17 -0700

    creating backend with express js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use((req, res, next) => {
+    console.log("first middleware");
+    next()
+})
+
+app.get('/', (req, res, next) => {
+    console.log("second middleware");
   res.send('Hello World!')
+//   next()
+})
+
+app.get('/two', (req, res) => {
+    console.log("third middleware");
+  res.send('Hello World 2!')
+})
+
+app.use((req,res) => {
+    console.log("fourth middleware");
 })
 
 app.listen(port, () => {
